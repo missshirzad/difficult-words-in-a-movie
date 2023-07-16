3306c076e141837a28093d9105865a3d34a53588
Miss.shirzad <129416654+missshirzad@users.noreply.github.com>
2023-07-16 12:24:47 +0430

    Update reading_srt2.py
    I define function for this code and create an input function for getting file

--- reading_srt.py
+++ reading_srt.py
@@ -1,30 +1,38 @@
-# Open and read the SRT file
-with open('C:\\Users\\DELL\\Desktop\\How.I.Met.Your.Mother.S01E01.srt', 'r') as srt_file:
-    srt_content = srt_file.read()
+#deffine a function for findding difference between SRT and TXT file
+def find_similar_words(srt_file, lc_file):
+    # Open and read the SRT file
+    with open(srt_file, 'r') as srt_file:
+        srt_content = srt_file.read()
+    
+    # Open and read the Longman Communication 3000 file
+    with open(lc_file, 'r') as lc_file:
+        lc_content = lc_file.read()
 
-# Split the content into words
-words = srt_content.split()
+    # Split the content into words
+    words = srt_content.split()
 
-# Create a list to store words without special characters
-clean_words = []
-for word in words:
-    if word.isalpha():
-        clean_words.append(word)
+    # Create a list to store words without special characters
+    clean_words = []
+    for word in words:
+    #use isalpha fun for separating alphabatical charecter of paragraph
+        if word.isalpha():
+            clean_words.append(word)
+    # Find similar words between the SRT file and Longman Communication 3000 file
+    similar_words = []
+    lc_words = lc_content.split()
+    for clean_word in clean_words:
+        for lc_word in lc_words:
+            if clean_word != lc_word.lower():
+                similar_words.append(lc_word)
 
-# Open and read the Longman Communication 3000 file
-with open('C:\\Users\\DELL\\Desktop\\Longman Communication 3000.txt', 'r') as lc_file:
-    lc_content = lc_file.read()
+    # Remove duplicates by converting the list to a set
+    synonyms = set(similar_words)
 
-# Find similar words between the SRT file and Longman Communication 3000 file
-similar_words = []
-lc_words = lc_content.split()
-for clean_word in clean_words:
-    for lc_word in lc_words:
-        if clean_word == lc_word:
-            similar_words.append(lc_word)
+    return synonyms
 
-# Remove duplicates by converting the list to a set
-synonyms = set(similar_words)
+# Get the file paths from user input
+srt_file = input("Enter the path to the SRT file as format (path_file, name_file.srt): ")
+lc_file = input("Enter the path to the Longman Communication 3000 file as format (path_file, name_file.txt): ")
 
-# Print the set of similar words
-print(synonyms)
+similar_words = find_similar_words(srt_file, lc_file)
+print(similar_words)

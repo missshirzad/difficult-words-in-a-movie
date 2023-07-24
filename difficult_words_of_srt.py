186514994c37ea09712297c68dbbb202b2c37b7a
Miss.shirzad <129416654+missshirzad@users.noreply.github.com>
2023-07-24 22:58:45 +0430

    added difficult_words_of_srt.py

--- difficult_words_of_srt.py
+++ difficult_words_of_srt.py
@@ -8,27 +8,34 @@ def find_unsimilar_words(srt_file_l, lc_file_l):
         words = srt_content.split()
 
         # Create a list to store words without special characters
-        clean_words = []
-        for word in words:
-            if word.isalpha():
-                clean_words.append(word)
+        clean_words = [word for word in words if word.isalpha()]
 
         # Open and read the Longman Communication 3000 file
         with open(lc_file_l, 'r') as lc_file:
             lc_content = lc_file.read()
-        
+
         # Split the content into words
         lc_words = lc_content.split()
 
-        # Find the unsimilar words (not present in the Longman Communication 3000 file)/ we use this simple word list
+        # Find the unsimilar words (not present in the Longman Communication 3000 file)
         unsimilar = []
+        difficult_word = []
+        clear = ['ing', 's','es', 'ed']
         for word in clean_words:
-            if word.lower()  in lc_words:
+            if word.lower() in lc_words:
                 pass
             else:
                 unsimilar.append(word.lower())
+        # find the words that end with (ing, es, s ,ed )suffix and remove them from new txt file if they are present in longman communication 3000
+        for word in unsimilar:
+            if len(word) == 1:
+                pass
+            elif word in lc_words and word.endswith(tuple(clear)):
+               pass
+            else:
+                difficult_word.append(word.lower())
 
-        return unsimilar
+        return unsimilar, difficult_word
     except FileNotFoundError:
         print("File not found. Please check the file paths.")
     except IOError:
@@ -36,13 +43,16 @@ def find_unsimilar_words(srt_file_l, lc_file_l):
 
 
 # Get the file paths from user input (You can replace these paths with your file paths)
-srt_file_l = input('please enter srt file path: ')
-lc_file_l = input('please enter longman communication 3000 file path: ')
+srt_file_l = input('please enter srt file  path: ')
+lc_file_l =  input('please enter Longman Communication 3000 file  path: ')
 # for testing code we use these links(https://github.com/jnoodle/English-Vocabulary-Word-List/blob/master/Longman%20Communication%203000.txt)
+
 # Call the function and retrieve the results
-unsimilar_words = find_unsimilar_words(srt_file_l, lc_file_l)
+unsimilar_words, difficult_words = find_unsimilar_words(srt_file_l, lc_file_l)
 
 # Write the unsimilar words to the 'new_txt_file.txt' file
 with open('new_txt_file.txt', 'w') as new_file:
-    for word in sorted(set(unsimilar_words)):  # Use set to remove duplicates
+    for word in sorted(set(difficult_words)):  # Use set to remove duplicates
         new_file.write(word + '\n')
+print(len(difficult_words))
+
